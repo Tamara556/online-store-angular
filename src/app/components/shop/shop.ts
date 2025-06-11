544face63a0bc1a658b5f7f2acc26a9964a3ca3f
Tamara Murazyan <murazyan.tamara5566.com>
2025-06-11 14:17:10 +0400

    shop changes

--- src/app/components/shop/shop.ts
+++ src/app/components/shop/shop.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Product} from '../../model/Product';
+import {ProductService} from '../../services/product';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './shop.html',
-  styleUrls: ['./shop.scss']
+  styleUrls: ['./shop.scss'],
 })
+export class ShopComponent implements OnInit {
 
-export class Shop {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => this.products = data,
+      error: (err) => console.error('Failed to load products', err)
+    });
+  }
 }
