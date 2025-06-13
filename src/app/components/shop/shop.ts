1188e4e613a1a2d1cafdbbf387704819622cce40
Tamara Murazyan <murazyan.tamara5566.com>
2025-06-13 12:58:50 +0400

    shop page modified

--- src/app/components/shop/shop.ts
+++ src/app/components/shop/shop.ts
@@ -1,26 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {Product} from '../../model/Product';
 import {ProductService} from '../../services/product';
-import {CommonModule} from '@angular/common';
-
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shop',
-  imports: [CommonModule],
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './shop.html',
   styleUrls: ['./shop.scss'],
 })
 export class ShopComponent implements OnInit {
 
   products: Product[] = [];
+  imageBaseUrl: string = 'http://localhost:8081/';
 
-  constructor(private productService: ProductService) {}
+  imageUrl: any;
+
+
+  constructor(private productService: ProductService,
+              private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
+    // this.productService.getAllProducts().subscribe({
+    //   next: (data) =>
+    //     this.products = data,
+    //   error: (err) => console.error('Failed to load products', err)
+    // });
+
     this.productService.getAllProducts().subscribe({
-      next: (data) => this.products = data,
-      error: (err) => console.error('Failed to load products', err)
+      next: (data) => {
+        console.log('Products loaded:', data); // ✅ log data
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Failed to load products:', err); // ❌ log error
+      }
     });
+
   }
+
+  // loadImage(fileName: string) {
+  //   this.productService.getProductImage(fileName).subscribe(blob => {
+  //     const objectURL = URL.createObjectURL(blob);
+  //     this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+  //   });
+  // }
 }
