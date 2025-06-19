d107056004c4a0b3026f71a7052544740b73b03e
Tamara556 <murazyan.tamara5566@gmail.com>
2025-06-19 23:55:34 +0400

    product service changes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Product} from '../model/Product';
+import {CreateProductModel} from '../model/create-product.model';
+import {UpdateProductModel} from '../model/update-product.model';
+import {AuthService} from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +19,17 @@ export class ProductService {
     return this.http.get<Product[]>(this.apiUrl);
   }
 
+  addProduct(product: CreateProductModel): Observable<Product> {
+    return this.http.post<Product>('http://localhost:8081/products', product);
+  }
+
+
+  updateProduct(product: UpdateProductModel): Observable<Product> {
+    return this.http.put<Product>(`http://localhost:8081/products/${product.id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`http://localhost:8081/products/${id}`);
+  }
 
 }
